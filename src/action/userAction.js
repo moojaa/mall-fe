40eea9af52e9fa3676e63543fccadc2d6b9fb087
moojaa <129938297+moojaa@users.noreply.github.com>
2024-05-30 22:39:05 +0900

    Update userAction.js

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -13,7 +13,7 @@ const registerUser =
   async (dispatch) => {
     try{
       dispatch({type:types.REGISTER_USER_REQUEST})
-      const response = await api.post("/users",{email,name,password})
+      const response = await api.post("/user",{email,name,password})
       if(response.status !==200)throw new Error(response.error)
       dispatch({type:types.REGISTER_USER_SUCCESS})
       dispatch(commonUiActions.showToastMessage("회원가입을 완료 했습니다","success"))
